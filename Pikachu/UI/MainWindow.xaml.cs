184fa5f38de552c16e9ceec052261ee57a2d8c75
kidevelop <hjh010501@naver.com>
2018-01-07 02:11:55 +0900

    Add Menu, ㅁnimation

--- Pikachu/UI/MainWindow.xaml.cs
+++ Pikachu/UI/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -19,15 +20,56 @@ namespace Pikachu
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        Storyboard MenuOn;
+        Storyboard MenuOff;
         public MainWindow()
         {
             InitializeComponent();
+            MenuOn = (Storyboard)FindResource("MenuOn");
+            MenuOff = (Storyboard)FindResource("MenuOff");
         }
 
-        private void Rectangle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        private void TitleButton_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((Grid)sender).Background = new SolidColorBrush(Color.FromRgb(30,30,32));
+        }
+
+        private void TitleButton_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Grid)sender).Background = new SolidColorBrush(Color.FromRgb(37,37,41));
+        }
+
+        private void CloseButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+        }
+
+        private void MenuButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MenuOff.Stop();
+            MenuOn.Begin();
+        }
+
+        private void BackButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MenuOn.Stop();
+            MenuOff.Begin();
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if(this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            } else if(this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
         }
     }
 }
